5e902d8f54c3b3316e3d4df326e95dc15627aecd
Christina Alimova <alimova.christina@gmail.com>
2018-10-03 23:11:45 +0300

    8 task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let count = 0;
+  let result = 0;
+  let resultArr = [];
+  let obj = {};
+  let currentDivider = 0;
+
+  for(let i = 2; i <= base;) {
+    if (base % i != 0) {
+      if (count != 0) {
+        obj[i] = count;
+        count = 0;
+      }
+      i++;
+      continue;
+    }
+    else {
+      base = base / i;
+      count++;
+      currentDivider = i;
+    }
+  }
+  obj[currentDivider] = count;
+  
+  for(let key in obj){
+    let tempNumber = number;
+    result = 0;
+    while(key <= tempNumber){
+      tempNumber = parseInt(tempNumber / key);
+      result = result + tempNumber;
+    }
+    result = parseInt(result / obj[key])
+    resultArr.push(result);
+  }
+
+  return Math.min.apply(null, resultArr);
 }
